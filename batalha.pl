e821342ae5b47c51df2c9d167cb7812e3ee793ff
Ruan Costa <ruan.cocito@gmail.com>
2013-09-03 16:46:33 -0300

    Brainstorming

--- batalha.pl
+++ batalha.pl
@@ -1,3 +1,57 @@
-#!/usr/bin/perl -w
+#! /usr/bin/perl 
 
 
+#open(CODMAQ, $ARGV[1]);
+
+#while(<STDIN>)
+#{
+  
+#}
+
+package Comando;
+
+sub novo
+{
+  my $class = shift;
+  my $self = {
+    opcode => shift,
+    valor => shift,
+  };
+  bless $self, $class;
+  return $self;
+}
+
+#sub set
+
+package Programa;
+
+sub novo
+{
+  my $class = shift;
+  my $self =
+  {
+    label => {},
+    vetor =>  [],
+    posi => 0,
+  };
+  bless $self, $class;
+  return $self;
+}
+
+sub novoComando
+{
+  $self = shift;
+  $self->{posi} = $self->posi+1;
+  $vetor = $self->{vetor};
+  push(@$vetor,shift);
+}
+
+#pog->novoComando(objComando);
+
+
+#package Maquina
+
+#sub novo
+#{
+
+#}
