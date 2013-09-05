a84bce6cb6373f1e290bac14c76c5038861c6ed4
Victor Portella <visportella@gmail.com>
2013-09-04 23:37:24 -0300

    Tranferi o tratamento da linha para o montador, e tava refinando o tratamento. Ou seja, fiz quase nada

--- Programa.pm
+++ Programa.pm
@@ -14,12 +14,95 @@ sub novo
   return $self;
 }
 
+sub interpretaLinha
+{
+  #Pega argumentos
+  my $self = shift;
+  my $linha = shift;
+
+  print "Linha $self->{posi}: $linha  || \n";
+
+  #$linha =~ s/\#.*//; #Elimina comentários
+
+
+
+  if($linha !~ /([a-zA-Z]+[a-zA-Z0-9]*\:)?(([a-zA-Z]+)\s+([a-zA-Z0-9]+))|([a-zA-Z])/)
+  #if(!/([a-zA-Z]+:)?(([a-zA-Z])+([0-9]+|[a-zA-Z]+)?)?/)
+    {print "Syntax error\n"}
+  else
+  {
+
+
+    if($1)
+    {
+      my $rotulo = $1;
+      chop($rotulo);#tirando':'
+
+      print "ACHEI ROTULO\n";
+      
+
+      if(!$self->existeLabel($rotulo))
+      {
+        $self->novoLabel($rotulo);
+      }
+      else
+      {
+        print "Redefinição de label.";
+        return 0; #fail
+      } 
+
+      print "Label :";#. $rotulo ."\n";
+    }
+
+    if($2)
+    {
+      $cmd = novo Comando($3, $4);
+    }
+
+    if($3)
+    {
+      $cmd = novo Comando($5, undef);
+    }
+    $self->insereComando($cmd); 
+    
+  }
+
+  return $self; #Recomendado.
+}
+
 sub insereComando
 {
   $self = shift;
   $self->{posi} = $self->{posi}+1;
   $vetor = $self->{vetor};
   push(@$vetor,shift);
+
+  return $self
+}
+
+
+sub existeLabel
+{
+  (my $self,my $label) = (@_);
+
+  my $labelsHash = $self->{label};
+
+  return 1 if($labelsHash->{$label} != undef );
+  return 0;
+
+}
+
+
+sub novoLabel
+{
+  (my $self,my $label,my $nLinha) = (@_);
+  my $labelsHash = $self->{label};
+
+  print "frita\n";
+
+  $labelsHash->{$label} = $nLinha;
+
+  return $self;
 }
 #pog->novoComando(objComando);
 1;
--- main.pl
+++ main.pl
@@ -1,38 +1,19 @@
 #! /usr/bin/perl -w
 
 use Comando;
-use Programa;
+use Programa;	
 use Maquina;
 package main;
 
 $maq = novo Maquina;
 $prog = novo Programa;
-$i = 0;
+
 while(<>)
 {
-	if(!/([a-zA-Z]+:)?(([a-zA-Z]+)\s+([a-zA-Z0-9]+))|([a-zA-Z])/)
-	#if(!/([a-zA-Z]+:)?(([a-zA-Z])+([0-9]+|[a-zA-Z]+)?)?/)
-		{print "Syntax error\n"}
-	else
-	{
-		if($1)
-		{
-			$rotulo = $1;
-			chop($rotulo);#tirando':'
-			$$prog{label}{$rotulo} = $i++;
-		}
-		if($2)
-		{
-			print "$rotulo $3 $4\n";
-			$cmd = novo Comando($3, $4);
-		}	
-		if($3)
-		{
-			$cmd = novo Comando($5, undef);
-		}
-		$prog->insereComando($cmd);	
-		
-	}
+
+	$prog->interpretaLinha($_);
+
+
 	#@linha = split(/ :/);
 	#prog->label{shift @linha} = $i++; #sintaxe provavelmente errada
 	#$$prog{label}{shift @linha} = $i++;
